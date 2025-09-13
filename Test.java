0ecf0b426987fc25fe2ba3189ecbcacde4523dcd
Manish Gupta <manishkarbon@gmail.com>
2025-09-13 16:16:23 +0530

    Initial commit

---
+++ Test.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Test {
+
+	public static void main(String[] args) {
+		 
+		System.out.println(new Date());
+		System.out.println("Testing is going on jenkins");
+
+	}
+
+}
